bbb0105b2057245695aa8eb13b5788d5b4ed4d2c
Rick Parrish <github@rick.parrish>
2013-07-09 10:10:39 -0400

    Fix windows install.cmd
    Now it only copies sbbsexec.dll (since the .exe's take care of cleaning
    up files they don't need), and it knows where sbbsexec.dll lives, even
    if the .cmd is run from a different directory (or run as administrator,
    which sometimes runs it from a different directory)

--- DLL/install.cmd
+++ DLL/install.cmd
@@ -1,8 +1,7 @@
 @ECHO OFF
-REM TODO UPDATE SO IT ACTUALLY WORKS
-REM (WHEN YOU RUN AS ADMINISTRATOR IT STARTS YOU IN C:\WINDOWS\SYSTEM32, SO NEED TO FIND A WAY TO KNOW WHAT DIRECTORY INSTALL.CMD WAS RUN FROM)
 
 REM Modified from http://stackoverflow.com/questions/4051883/batch-script-how-to-check-for-admin-rights
+REM Get current directory from http://stackoverflow.com/questions/130112/command-line-cmd-bat-script-how-to-get-directory-of-running-script
 
 REM Run NET SESSION and check the errorlevel to see if we're running with admin priviledge
 NET SESSION >nul 2>&1
@@ -25,14 +24,10 @@ IF %ERRORLEVEL% NEQ 0 (
    EXIT /B 1
 )
 
-del cpulimit.sh
-del dosutils.zip
-del install.sh
-del pty-sharp-1.0.tgz
-del start.sh
+REM copy sbbsexec.dll to the windows\system32 directory (needed to run dos doors under Vista and newer, doesn't hurt on XP)
+copy %~dp0sbbsexec.dll %windir%\system32\sbbsexec.dll
 
-copy sbbsexec.dll %windir%\system32\sbbsexec.dll
-
-start GameSrvGUI.exe
+REM start the GUI version of GameSrv
+start %~dp0GameSrvGUI.exe
 
 @echo ON
