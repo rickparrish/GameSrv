4ce8a61cab312bb50561a387142e39485569bc2b
rickparrish <github@rick.parrish>
2015-09-22 19:32:36 -0400

    Add script to update binaries from git

---
+++ SupportFiles/update_from_git.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+wget -O GameSrv.dll https://github.com/rickparrish/GameSrv/raw/master/bin/Release/GameSrv.dll
+wget -O GameSrvConfig.exe https://github.com/rickparrish/GameSrv/raw/master/bin/Release/GameSrvConfig.exe
+wget -O GameSrvConsole.exe https://github.com/rickparrish/GameSrv/raw/master/bin/Release/GameSrvConsole.exe
+wget -O GameSrvGUI.exe https://github.com/rickparrish/GameSrv/raw/master/bin/Release/GameSrvGUI.exe
+wget -O GameSrvService.exe https://github.com/rickparrish/GameSrv/raw/master/bin/Release/GameSrvService.exe
+wget -O RMLib.dll https://github.com/rickparrish/GameSrv/raw/master/bin/Release/RMLib.dll
+wget -O RMLibUI.dll https://github.com/rickparrish/GameSrv/raw/master/bin/Release/RMLibUI.dll
+wget -O Upgrade.exe https://github.com/rickparrish/GameSrv/raw/master/bin/Release/Upgrade.exe
