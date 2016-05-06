ba91148e75bb69acc9186a9444db9c96bafaa40e
rickparrish <github@rick.parrish>
2016-05-06 11:23:04 -0400

    Make powershell script self-update

--- SupportFiles/update_from_git.ps1
+++ SupportFiles/update_from_git.ps1
@@ -3,13 +3,18 @@ function DownloadFile($filename) {
 	Invoke-WebRequest "https://github.com/rickparrish/GameSrv/raw/master/bin/Release/$($filename)" -OutFile $filename
 }
 
-DownloadFile("GameSrv.dll")
-DownloadFile("GameSrvConfig.exe")
-DownloadFile("GameSrvConsole.exe")
-DownloadFile("GameSrvGUI.exe")
-DownloadFile("GameSrvService.exe")
-DownloadFile("RMLib.dll")
-DownloadFile("RMLibUI.dll")
-DownloadFile("Upgrade.exe")
-DownloadFile("W32Door.exe")
-DownloadFile("update_from_git.ps1")
+if ($args.count -eq 0) {
+    Write-Host "Downloading latest update_from_git.ps1 file"
+    DownloadFile("update_from_git.ps1")
+    Invoke-Expression -Command ".\update_from_git.ps1 DOWNLOAD"
+} else {
+    DownloadFile("GameSrv.dll")
+    DownloadFile("GameSrvConfig.exe")
+    DownloadFile("GameSrvConsole.exe")
+    DownloadFile("GameSrvGUI.exe")
+    DownloadFile("GameSrvService.exe")
+    DownloadFile("RMLib.dll")
+    DownloadFile("RMLibUI.dll")
+    DownloadFile("Upgrade.exe")
+    DownloadFile("W32Door.exe")
+}
